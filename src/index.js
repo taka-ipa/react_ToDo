676cf24f50cb23b08a847c10ed89a513c9311eed
taka-engy <sake.sloth1110@gmail.com>
2024-01-21 19:35:01 +0900

    js review

---
+++ src/index.js
@@ -0,0 +1,115 @@
+// const, let等の変数宣言
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = 'let変数';
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = 'let変数を上書き';
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = 'let変数を再宣言';
+// console.log(val2);
+
+// const val3 = 'const変数';
+// console.log(val3);
+
+// // const変数は上書き不可
+// const val3 = '上書き不可';
+
+// constで定義したオブジェクトはプロパティの変更は可能
+// const val4 =  {
+//   name: 'じゃけぇ',
+//   age: 31
+// };
+// val4.name = 'jak';
+// val4.address = 'hiroshima';
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ['dog', 'cat'];
+// val5[0] = 'bird';
+// val5.push('monkey');
+// console.log(val5);
+
+// テンプレート文字列
+// const name = 'じゃけぇ';
+// const age = 31;
+
+// 「私の名前はじゃけぇです。年齢は31歳です。」
+
+// 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+// テンプレート文字列を用いた方法
+// const message2 =  `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// const func3 = (num1,num2) => ({
+//   hoge: num1,
+//   huga: num2
+// });
+// console.log(func3(1, 2));
+
+// 分割代入
+
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 31
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// const myProfile = ["じゃけぇ", 31];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+// オブジェクトの定義
+// const name = "じゃけぇ";
+// const age = 31;
+
+// const myProfile = { name, age };
+// console.log(myProfile);
+
+// スプレッド構文
+
+// 配列の展開
+// const arr1 = [1,2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// 配列のコピー、結合
+// const arr4 = [10,20];
+// const arr5 = [30,40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
