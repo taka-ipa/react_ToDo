344ce52b7249826bc975ca2fb0eac44b186b97dc
taka-engy <sake.sloth1110@gmail.com>
2024-01-23 23:55:01 +0900

    js review2

--- src/index.js
+++ src/index.js
@@ -113,3 +113,32 @@
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
+
+// mapやfilterを使った配列の処理
+
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(name));
+
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "じゃけぇ") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr);
